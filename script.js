c260d2ed1862494f3c4909d84a62f843fcaa7416
nicoll-douglas <nicoll.leo.douglas@gmail.com>
2023-12-19 09:56:58 +0000

    Complete header for desktop and mobile.

--- script.js
+++ script.js
@@ -15,20 +15,48 @@
   })
 })();
 
+
+const mediaQuery = (function () {
+  const desktop = window.matchMedia("(min-width: 1124px)");
+  const mobile = window.matchMedia("(max-width: 1124px)");
+
+  return {desktop, mobile};
+})();
+
+
 const focusEffects = (function () {
   let hoverItems = document.querySelectorAll(".hover-item");
 
+  (function clearMobileFocus() {
+    window.addEventListener("resize", () => {
+      if (mediaQuery.mobile.matches) {
+        hoverItems.forEach(item => {
+          item.classList.remove("hoverItemBorder");
+        })
+      }
+      if(mediaQuery.desktop.matches) {
+        document.querySelectorAll(".icon").forEach(chevron => {
+          chevron.classList.remove("rotate180");
+        })
+      }
+    })
+  })();
+
   const removeFocusEffect = () => {
     hoverItems.forEach(item => {
-      item.classList.remove("hoverItemBorder");
+      if (mediaQuery.desktop.matches) {
+        item.classList.remove("hoverItemBorder");
+      }
     })
   }
 
   (function addFocusEffect() {
     hoverItems.forEach(item => {
       item.addEventListener("click", () => {
-        removeFocusEffect();
-        item.classList.add("hoverItemBorder");
+        if (mediaQuery.desktop.matches) {
+          removeFocusEffect();
+          item.classList.add("hoverItemBorder");
+        }
       })
     })
   })();
@@ -36,10 +64,36 @@ const focusEffects = (function () {
   return {remove: removeFocusEffect};
 })();
 
+
 const dropdown  = (function () {
   let dropdowns = document.querySelectorAll(".dropdown");
   let dropContent = document.querySelectorAll(".drop-content");
 
+  const chevronEffects = (function () {
+    let chevrons = document.querySelectorAll(".icon");
+
+    function rotate(i) {
+      chevrons[i].classList.toggle("rotate180");
+    }
+
+    function reset() {
+      chevrons.forEach(chevron => {
+        chevron.classList.remove("rotate180");
+      })
+    }
+
+    for (let i = 0; i < dropdowns.length; i++) {
+      dropdowns[i].addEventListener("click", () => {
+        if (mediaQuery.mobile.matches) {
+          if ([...dropContent].some(element => !element.matches("viewHidden")) && dropContent[i].classList.contains("viewHidden")) reset();
+          rotate(i);
+        }
+      })
+    }
+
+    return {reset}
+  })();
+  
   function hideDropdowns() {
     dropContent.forEach(element => {
       element.classList.add("viewHidden");
@@ -49,23 +103,43 @@ const dropdown  = (function () {
   (function toggleDropdown() {
     for (let i = 0; i < dropdowns.length; i++) {
       dropdowns[i].addEventListener("click", () => {
-        if ([...dropContent].some(element => !element.matches("viewHidden")) && dropContent[i].classList.contains("viewHidden")) {
-          hideDropdowns();
-          dropContent[i].classList.toggle("viewHidden");
-        } else {
-          dropContent[i].classList.toggle("viewHidden")
-        }
+        if ([...dropContent].some(element => !element.matches("viewHidden")) && dropContent[i].classList.contains("viewHidden")) hideDropdowns();
+        dropContent[i].classList.toggle("viewHidden");
       })
     }
   })();
 
   (function hideIfClickAway() {
     window.addEventListener("click", e => {
-      if (!e.target.matches("header, header *")) hideDropdowns();
+      if (!e.target.matches("header, header *")) {
+        hideDropdowns();
+        chevronEffects.reset();
+      }
       if (!e.target.matches(".dropdown")) focusEffects.remove();
    })
   })();
 })();
 
-// 1. Click on ".dropdown[i]" => dropdownActive => forEach ".drop-content" Add ".viewHidden" => Toggle ".drop-content[i]"
-// 2. Click on ".dropdown[i]" => !dropdownActive => Toggle ".drop-content[i]"
+
+(function toggleMenu() {
+  let nav = document.querySelector("nav")
+  let menuLabel = document.querySelector("#menu-label");
+  
+  (function closeMenu() {
+    let closeIcon = document.querySelector("#close-menu");
+
+    closeIcon.addEventListener("click", () => {
+      nav.classList.remove("flexDisplay");
+      menuLabel.classList.remove("flexDisplay");
+    })
+  })();
+
+  (function openMenu() {
+    let openBtn = document.querySelector("#menu");
+
+    openBtn.addEventListener("click", () => {
+      nav.classList.add("flexDisplay");
+      menuLabel.classList.add("flexDisplay");
+    })
+  })();
+})();
